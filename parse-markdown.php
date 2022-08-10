7553a5d75f45e2083055dca98738ba20919cd1d0
Wojciech Frącz <fraczwojciech@gmail.com>
2022-08-10 12:34:28 +0200

    Add JS & CSS

--- parse-markdown.php
+++ parse-markdown.php
@@ -11,7 +11,7 @@ if (!$path || !is_readable($path)) {
     exit;
 }
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 $contents = file_get_contents($path);
 
@@ -53,8 +53,8 @@ $body = str_replace('language-', 'line-numbers language-', $body);
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= $title ?></title>
-    <link rel="stylesheet" href="/css/github-markdown.css">
-    <link rel="stylesheet" href="/css/prism.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.1.0/github-markdown.min.css" integrity="sha512-KUoB3bZ1XRBYj1QcH4BHCQjurAZnCO3WdrswyLDtp7BMwCw7dPZngSLqILf68SGgvnWHTD5pPaYrXi6wiRJ65g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/themes/prism.min.css" integrity="sha512-/mZ1FHPkg6EKcxo0fKXF51ak6Cr2ocgDi5ytaTBjsQZIH/RNs6GF6+oId/vPe3eJB836T36nXwVh/WBl/cWT4w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="/css/micromodal.css">
     <style>
         .markdown-body {
@@ -88,7 +88,7 @@ $body = str_replace('language-', 'line-numbers language-', $body);
     <?= $body; ?>
     <div class="footer">Wojciech Frącz, fracz@agh.edu.pl</div>
 </div>
-<script src="/css/prism.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js" integrity="sha512-UOoJElONeUNzQbbKQbjldDf9MwOHqxNz49NNJJ1d90yp+X9edsHyJoAs6O4K19CZGaIdjI5ohK+O2y5lBTW6uQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://unpkg.com/micromodal/dist/micromodal.min.js"></script>
 <script>
     MicroModal.init();
